73138cdde56c88e21dda75fdec0f7b7203da6b1f
anudeepsetty <103600304+anudeepsetty@users.noreply.github.com>
2022-04-14 00:04:03 +0530

    Add files via upload

---
+++ Get-CommonTimeZone_Fn.ps1
@@ -0,0 +1,44 @@
+﻿
+
+function Get-CommonTimeZone 
+{
+
+    param(
+            [validaterange(-12,+12)]
+            [string]$offset,
+            [string]$Value
+         )
+         if($offset.length -gt 0 -and $Value -gt 0)
+         {
+            Write-Error 'Input Parameters are not allowed to supply simulataneously'
+         }
+         else
+         {
+            $TimeZoneData= Invoke-RestMethod 'https://raw.githubusercontent.com/dmfilipenko/timezones.json/master/timezones.json'
+            
+            $offset
+
+
+            if($Value.Length -gt 0)
+            {
+                Write-Host "value"
+
+                if($TimeZoneData | Where value -Like *$value*)
+                {
+                    $TimeZoneData | Where value -Like *$value* | Format-Table
+                }
+                else
+                {
+                    Write-Host "The Specified Value $Value doesn't match with any value in GitHub TimeZone Data"
+                }
+            }
+            elseif($offset.length -gt 0)
+            {
+                    $TimeZoneData | Where offset -eq $offset | Format-Table
+            }
+            else
+            {
+                $TimeZoneData |Format-Table
+            }
+          }
+}
